f51299b7e25546fff89acf49912c6c768d680cd1
imorenodev@gmail.com <imorenodev@gmail.com>
2016-06-09 23:06:24 -0400

    need to complete facebook strategy

--- rest-server-passport/authenticate.js
+++ rest-server-passport/authenticate.js
@@ -1,8 +1,11 @@
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 var User = require('./models/user');
 var config = require('./config');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+// NEED TO ADD exports.facebook Facebook Strategy
--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
   'secretKey': '12345-67890-09876-54321',
-  'mongoUrl': 'mongodb://localhost:27017/conFusion'
+  'mongoUrl': 'mongodb://localhost:27017/conFusion',
+  'facebook': {
+    clientID: '169053966828978',
+    clientSecret: '711eb0a60fa081c8cc17052a23a34e8c',
+    callbackURL: 'https://localhost:3443/users/facebook/callback'
+  }
 };
