5581cd288d8c5197b02b3b3fa8e875f9899dfab2
imorenodev@gmail.com <imorenodev@gmail.com>
2016-06-10 11:28:13 -0400

    check for Oauth user in db, if not found, create new user

--- rest-server-passport/authenticate.js
+++ rest-server-passport/authenticate.js
@@ -8,4 +8,38 @@ exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-// NEED TO ADD exports.facebook Facebook Strategy
+exports.facebook = passport.use(new FacebookStrategy(
+  {
+    clientID: config.facebook.clientSecret,
+    clientSecret: config.facebook.clientSecret,
+    callbackURL: config.facebook.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // check if user already exists in db
+    User.findOne({ OauthId: profile.id }, 
+      function(err, user) {
+        if (err) {
+          console.log(err); // handle errors
+        }
+        if (!err && user !== null) {
+          // if user is already in db then return user
+          done(null, user);
+        } else {
+          // create new user
+          user = new User({
+            username: profile.displayName
+          });
+          user.OauthId = profile.id;
+          user.OauthToken = accessToken;
+          user.save(function(err) {
+            if (err) {
+              console.log(err);
+            } else {
+              console.log('saving user ...');
+              done(null, user);
+            }
+          });
+        }
+      })
+  }
+));
